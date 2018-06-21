ea188e9cd03e3cb16ca7667892be30b2faf8cc9c
Cole Chamberlain <cole.chamberlain@gmail.com>
2018-06-21 11:58:38 -0700

    added try / catch to prevent total build failure

--- index.js
+++ index.js
@@ -30,44 +30,49 @@ module.exports = function (source) {
   this.cacheable && this.cacheable();
   var callback = this.async();
 
-  if (this.resourcePath.indexOf('node_modules') !== -1) {
-    // [ "/my/project/", "/moment/", "/moment-dependency/index.js" ]
-    var nodeModuleSplit = this.resourcePath.split('node_modules');
-    // "/my/project/node_modules/moment/node_modules/"
-    var packageNodeModulesDir = path.join(nodeModuleSplit.slice(0, nodeModuleSplit.length - 1).join('node_modules'), 'node_modules');
-    // "moment-dependency/index.js"
-    var relativeFilePath = nodeModuleSplit[nodeModuleSplit.length - 1].replace(/^[/\\]*/, '');
-    // "moment-dependency"
-    var relativePackageDir = relativeFilePath.replace(/[/\\].*$/i, '');
-    // "index.js"
-    var relativeSubFilePath = relativeFilePath.replace(/[/\\].*$/i, '');
-    // "/my/project/node_modules/moment/node_modules/moment-dependency"
-    var packageDir = path.resolve(packageNodeModulesDir, relativePackageDir);
-    var packageJSON = require(path.join(packageDir, 'package.json'));
+  try {
+    if (this.resourcePath.indexOf('node_modules') !== -1) {
+      // [ "/my/project/", "/moment/", "/moment-dependency/index.js" ]
+      var nodeModuleSplit = this.resourcePath.split('node_modules');
+      // "/my/project/node_modules/moment/node_modules/"
+      var packageNodeModulesDir = path.join(nodeModuleSplit.slice(0, nodeModuleSplit.length - 1).join('node_modules'), 'node_modules');
+      // "moment-dependency/index.js"
+      var relativeFilePath = nodeModuleSplit[nodeModuleSplit.length - 1].replace(/^[/\\]*/, '');
+      // "moment-dependency"
+      var relativePackageDir = relativeFilePath.replace(/[/\\].*$/i, '');
+      // "index.js"
+      var relativeSubFilePath = relativeFilePath.replace(/[/\\].*$/i, '');
+      // "/my/project/node_modules/moment/node_modules/moment-dependency"
+      var packageDir = path.resolve(packageNodeModulesDir, relativePackageDir);
+      var packageJSON = require(path.join(packageDir, 'package.json'));
 
-    const fingerprint = `${packageJSON.name}:${packageJSON.version}`
-    if(cache.indexOf(fingerprint) !== -1)
-      return callback(null, source);
-    cache.push(fingerprint);
+      const fingerprint = `${packageJSON.name}:${packageJSON.version}`
+      if(cache.indexOf(fingerprint) !== -1)
+        return callback(null, source);
+      cache.push(fingerprint);
 
-    fs.readdir(packageDir, (err, files) => {
-      if(err) return callback(err);
-      let handled = false;
-      for(var licenseFileName of licenseFileNames) {
-        if(files.indexOf(licenseFileName) !== -1) {
-          var licensePath = path.resolve(packageDir, licenseFileName);
-          fs.readFile(licensePath, { encoding: 'utf8' }, (licenseErr, licenseText) => {
-            if(licenseErr) callback(licenseErr);
-            else callback(null, `${licenseComment(packageJSON, `==${packageJSON.name}/${licenseFileName}==\n${licenseText}`)}\n${source}`);
-          })
-          handled = true;
-          break;
+      fs.readdir(packageDir, (err, files) => {
+        if(err) return callback(err);
+        let handled = false;
+        for(var licenseFileName of licenseFileNames) {
+          if(files.indexOf(licenseFileName) !== -1) {
+            var licensePath = path.resolve(packageDir, licenseFileName);
+            fs.readFile(licensePath, { encoding: 'utf8' }, (licenseErr, licenseText) => {
+              if(licenseErr) callback(licenseErr);
+              else callback(null, `${licenseComment(packageJSON, `==${packageJSON.name}/${licenseFileName}==\n${licenseText}`)}\n${source}`);
+            })
+            handled = true;
+            break;
+          }
         }
-      }
-      // No license found, use whatever is in package.json
-      if(!handled) callback(null, `${licenseComment(packageJSON)}\n${source}`);
-    })
-  } else {
+        // No license found, use whatever is in package.json
+        if(!handled) callback(null, `${licenseComment(packageJSON)}\n${source}`);
+      })
+    } else {
+      callback(null, source);
+    }
+  } catch (err) {
+    console.error("license-loader: An error occurred reading package, skipping...", err);
     callback(null, source);
   }
 }
